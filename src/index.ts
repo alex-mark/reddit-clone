a72d2fab02974125998fd9edf09a41701a59aaf3
Alex Markin <alexndr86@gmail.com>
2020-10-30 17:51:23 +0300

    Add post model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "reddit-clone",
+    user: "",
+    password: "",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
+
 console.log("hello world");
