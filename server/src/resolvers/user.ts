f5fa6aae99aa5c232197820cb68ca0e5dd2d9128
Alex Markin <alexndr86@gmail.com>
2020-11-02 21:27:10 +0300

    Fix user resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
@@ -67,7 +68,7 @@ export class UserResolver {
         ],
       };
     }
-    const hashedPassword = await argon2.hash(options.password);
+
     if (options.password.length <= 3) {
       return {
         errors: [
@@ -78,12 +79,21 @@ export class UserResolver {
         ],
       };
     }
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+
+    const hashedPassword = await argon2.hash(options.password);
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       if (err.code === "23505" || err.detail.includes("already exists")) {
         return {
