3d24fec9022f2ba6f506c0342e49b2fcf879e8a3
Alex Markin <alexndr86@gmail.com>
2020-11-02 18:20:58 +0300

    Add register page

---
+++ web/src/components/Wrapper.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/core";
+import React, { ReactElement } from "react";
+
+interface WrapperProps {
+  variant?: "small" | "regular";
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => (
+  <Box
+    mt={8}
+    mx="auto"
+    maxW={variant === "regular" ? "800px" : "400px"}
+    w="100%"
+  >
+    {children}
+  </Box>
+);
+
+export default Wrapper;
