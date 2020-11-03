fc7a0f67ab8ccf9df060c022e2f7a5056c4d3491
Alex Markin <alexndr86@gmail.com>
2020-11-03 16:26:26 +0300

    Refactor for ssr

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { withUrqlClient } from "next-urql";
 import React from "react";
 import NavBar from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-    <NavBar />
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient)(Index);
