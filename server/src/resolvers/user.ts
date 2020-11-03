896ed619e64f40666d3a90dc2b519c9c75f8a26c
Alex Markin <alexndr86@gmail.com>
2020-11-03 16:04:31 +0300

    Update logout

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -8,10 +8,12 @@ import {
   Mutation,
   ObjectType,
   Query,
+  ReflectMetadataMissingError,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -144,5 +146,21 @@ export class UserResolver {
     req.session.userId = user.id;
 
     return { user };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
   }
 }
