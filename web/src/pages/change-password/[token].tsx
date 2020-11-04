dcec4afe360216c1ea8a2bf15fd6684277a867a0
Alex Markin <alexndr86@gmail.com>
2020-11-04 14:01:16 +0300

    Add change password mutation

---
+++ web/src/pages/change-password/[token].tsx
@@ -0,0 +1,54 @@
+import { Box, Button } from "@chakra-ui/core";
+import { Formik, Form } from "formik";
+import { NextPage } from "next";
+import React from "react";
+import { InputField } from "../../components/InputField";
+import Wrapper from "../../components/Wrapper";
+import { toErrorMap } from "../../utils/toErrorMap";
+import register from "../register";
+
+const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          // const response = await register({ options: values });
+          // if (response.data?.register.errors) {
+          //   setErrors(toErrorMap(response.data.register.errors));
+          // } else if (response.data?.register.user) {
+          //   router.push("/");
+          // }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="new password"
+              label="New password"
+              type="password"
+            />
+
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              change password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
